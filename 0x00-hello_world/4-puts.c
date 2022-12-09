cac9ac92faa86718a6a3089b389fc05c081f69b2
kofikg <kofiivan86@gmail.com>
2022-12-08 19:06:05 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  * Return: always 0 (success)
  */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+ {
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
  }
