fd1fb47c7a297ad075ffd1ee5208cdb2f595c962
kofikg <kofiivan86@gmail.com>
2022-12-09 15:48:48 -0800

    0-positive_or_negative 1

--- 0-positive_or_negative.c/0-positive_or_negative.c
+++ 0-positive_or_negative.c/0-positive_or_negative.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+#include <stdlib.h>
+
+#include <time.h>
+
 /**
- * main - Entry point
  *
- * Return: always 0 (success)
+ *  * main - main block
+ *
+ *   * Description: prints "is it positive or negative"
+ *
+ *    * Return: 0
  */
 
 int main(void)
+
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if  (n == 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
-	return (0);
-}
 
+
+srand(time(0));
+
+n = rand() - RAND_MAX / 2;
+
+
+
+if (n > 0)
+
+printf("%d is positive\n", n);
+
+else if (n < 0)
+
+printf("%d is negative\n", n);
+
+else
+
+printf("%d is zero\n", n);
+return (0);
+}
