1f27b077ee3c04f74b6ccae8fbde58dfda4eca0d
kofikg <kofiivan86@gmail.com>
2022-12-14 07:51:39 -0800

    Sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,19 +4,17 @@
  * _isaipha - Return 1 if c is a letter. lowercase or uppercase
  *
  * @c: The int to print
+ 
  * Return: Always 0.
  */
-
-int _isalpha(int c)
-
+int _isalpha(int c);
 {
-	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return (1);
+	      	return (1);
 	}
-
 	else
 	{
-		return (0);
+                return (0);
 	}
-}
+}	
