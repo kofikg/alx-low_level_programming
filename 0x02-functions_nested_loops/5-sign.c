67e275db2d35f7004694b007181eb8bff544a645
kofikg <kofiivan86@gmail.com>
2022-12-14 15:54:51 -0800

    sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowercase
- *
- * @n: number to check
- * Return: 0 or 1
+ * print_sign - function that prints the sign of a number..
+ * @n: The input number to check
+ * Return: int
  */
+
 int print_sign(int n)
 {
-	int test;
-
 	if (n > 0)
 	{
-		test = 1;
-		_putchar('+');
+		_putchar(43);
+		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		test = 0;
-		_putchar('0');
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		test = -1;
-		_putchar('_');
+		_putchar(48);
+		return (0);
 	}
-	return (test);
 }
+
 
