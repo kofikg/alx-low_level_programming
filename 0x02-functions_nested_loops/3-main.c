de0e9b68ea8bd66fc155e15a46db652eae881dea
kofikg <kofiivan86@gmail.com>
2022-12-14 06:25:13 -0800

    Lower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -16,23 +16,23 @@ int main(void)
 
 {
 
-	    int r;
+ int r;
 
+r = _islower('H');
 
+_putchar(r + '0');
 
-	        r = _islower('H');
+r = _islower('o');
 
-		    _putchar(r + '0');
+_putchar(r + '0');
 
-		        r = _islower('o');
+r = _islower(108);
 
-			    _putchar(r + '0');
+_putchar(r + '0');
 
-			        r = _islower(108);
+_putchar('\n');
 
-				    _putchar(r + '0');
+ return (0);
 
-				        _putchar('\n');
-
-					    return}
+}
 
