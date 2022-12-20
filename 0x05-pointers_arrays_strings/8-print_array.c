bdcc06ebcc23b5b1164f215d3e505d34601114ee
kofikg <kofiivan86@gmail.com>
2022-12-19 17:40:41 -0800

    Rays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,9 +16,8 @@ void print_array(int *a, int n)
 	{
 		printf("%d", a[count]);
 
-		if (count != (n - 1));
-		         printf(", ");
-		count++; 
+		if (count != (n - l))
+			printf(", "); count++;
 	}
 	putchar('\n');
 }
