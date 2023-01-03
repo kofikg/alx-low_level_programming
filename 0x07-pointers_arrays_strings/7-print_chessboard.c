8968fdaac85e1a6eaa0b2745e5a13be97bb26d15
kofikg <kofiivan86@gmail.com>
2023-01-03 15:48:46 -0800

    diagsums

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * print_chessboard - prints a classboard.
- * @a: The chessboard to be printed.
+ *print_chessboard - Entry poin
+ *@a: array 
+ *Return: Always 0 (success)
  */
 void print_chessboard(char (*a)[8])
 {
-int indx1, indx2;
-
-for (indx1 = 0; a[indx1][7]; index1++)
+int i;
+int j;
+for (i = 0; i < 8; i++)
 {
-for (indx2 = 0; indx2 < 8; indx2++)
-_putchar(a[indx1][indx2]);
+for (j = 0; j < 8; j++)
+_putchar(a[i][j]);
 _putchar('\n');
 }
