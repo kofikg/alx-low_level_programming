8c35891b9531d81c3a690cdc07164a277786c16e
kofikg <kofiivan86@gmail.com>
2023-01-06 02:16:53 -0800

    palindrom

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,49 @@
 #include "main.h"
 
-int find_strlen(char *s);
-int check_palidrome(char *s, int len, int index);
-int is_palidrome(char *s);
+/**
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+}
 
 /**
- * find_strlen - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * pl - palidrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
  */
-int find_strlen(char *s)
+
+int pl(char *s, int l)
 {
-int len = 0;
-if (*(s + len))
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (pl(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
 {
+	int len = _strlen_recursion(s);
+
+	return (pl(s, len - 1));
+}
