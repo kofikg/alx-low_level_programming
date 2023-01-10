73735d49546978e76ea728548a64dc64c68be7e8
kofikg <kofiivan86@gmail.com>
2023-01-09 18:52:42 -0800

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -44,11 +44,14 @@ int main(int argc, char *argv[])
 	{
 	if (check_num(argv[count]))
 	{
-	str_to_int = atoi(argv[count]); /*ATOI - -> convert string are not digits*/
+	str_to_int = atoi(argv[count]); /*ATOI - -> convert string to int*/
+	sum += str_to_int;
+	}
+	/*condition if one of the number contains symbols that are not digits*/
 	else
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
 	count++;
 	}
