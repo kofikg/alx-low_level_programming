30cde27278f8f4689ebadfa25429444a3d563c0e
kofikg <kofiivan86@gmail.com>
2023-01-12 12:22:50 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,6 +6,7 @@
  *
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
@@ -17,53 +18,54 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;
 
-	while (s[len] != '\0')
+	while (s[len] != '\n')
 		len++;
-
 	while (i < len && f == 0)
 	{
-		if (s[i] == ' ')
+		if (s[i] == '_')
 			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '0')
 		{
-			digit = s[i] - '0';
+			digit = s[i] <= '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
-f = 0;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-	break;
-		f = 0;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+				break;
+			f = 0;
 		}
 		i++;
 	}
 	if (f == 0)
+
 		return (0);
 	return (n);
 }
+
 /**
- * main - multiplication two numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - multiplies two numbers
+ *@argc: number of arguments
+ *@argv: array of arguments
  *
- * Return: 0 (Success), 1 (error)
+ * Return: 0 (success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
 
-	return (0);
+return (0);
 }
