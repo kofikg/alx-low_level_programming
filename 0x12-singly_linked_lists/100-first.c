2a6cbd7d2f790611cf33920ac787524466b390b5
yaaaW777 <asanteantwi@gmail.com>
2023-01-26 07:39:12 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) here(void);
+
+/**
+ * here - prints a string before the
+ *        main function is excuted.
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,"
+
+		"\nI bore my house upon my back!\n");
+}
+
+
